5b42aed0f93055d5bf30d11ea3e605256cc76fe3
Saeed Yazdinezhad <saeedyazdinezhad@gmail.com>
2023-03-28 16:42:40 -0700

    remove unneccesery lines of EVSPFinder script

--- src/EVSP_Finder/EVSPFinder.ts
+++ src/EVSP_Finder/EVSPFinder.ts
@@ -7,26 +7,6 @@ import {
   SetEVPriceTime,
 } from "../abstract/maching.js";
 
-// Sample data:
-const evs = [
-  {
-    name: "Vehicle 1",
-    owner: true,
-    type: "ElectricVehicle",
-    brand: "Tesla",
-    range: "402 miles",
-    color: "blue",
-    model: "Model S",
-    year: "2022",
-    VIN: "5YJSA1E11MF413982",
-    batteryCapacity: 75,
-    chargingRate: 150,
-    currentCharge: 70,
-    chargingSpeed: 100,
-    location: { latitude: 37.7749, longitude: -122.4194 },
-  },
-];
-
 async function finder(EV, priority, price, time) {
   setPriority(priority);
   SetEVPriceTime(price, time);
@@ -42,7 +22,6 @@ async function finder(EV, priority, price, time) {
   }
 
   const graph = generateWeightedGraph(EV, temp, priority);
-  //   console.log("HH", findBestMatchingEVSP(graph, EV[0].name));
   return findBestMatchingEVSP(graph, EV[0].name);
 }
 
