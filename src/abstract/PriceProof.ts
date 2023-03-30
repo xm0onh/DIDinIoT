61f1e991c298f8a8424221f476844ffbd50737bb
Saeed Yazdinezhad <saeedyazdinezhad@gmail.com>
2023-03-29 23:48:52 -0700

    add measuring the duration

--- src/abstract/PriceProof.ts
+++ src/abstract/PriceProof.ts
@@ -9,17 +9,19 @@ import { DeployVerifierContract } from "../privacy/deployContract.js";
 import { agent } from "../veramo/setup.js";
 
 async function PriceProof(EV, EVSP) {
-  let EV_VC = (await agent.dataStoreGetVerifiableCredential({ hash: EV }))
-    .credentialSubject;
-  let EVSP_VC = (await agent.dataStoreGetVerifiableCredential({ hash: EVSP }))
-    .credentialSubject;
   //   const price = [
   //     JSON.stringify(EV_VC.desiredPrice * 10),
   //     JSON.stringify(EVSP_VC.price * 10),
   //   ];
+  const start = new Date().getTime();
+  let elapsed = 0;
   const price = ["4", "16"];
   await GenerateProof(price);
-  const out = await DeployVerifierContract("price", EV, EVSP);
+  const out = await DeployVerifierContract("price", EV, EVSP).then(() => {
+    elapsed = new Date().getTime() - start;
+  });
+  console.log("------> Elapsed", elapsed / 1000);
+
   return out;
 }
 
